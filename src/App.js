58c9da3e46b71d3e8c2b491cc0e10dceea66e00d
Vivek Ramteke <vivek2561@gmail.com>
2023-04-08 21:56:50 +0530

    Assignement

--- src/App.js
+++ src/App.js
@@ -1,25 +1,167 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
+import Comment from "./Comment";
+import { useState, useEffect } from "react";
+import data from "./data.json";
+import TextArea from "./Textarea";
+let j = 9201921;
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+
+export default function App() {
+  const getLocalItems = () => {
+    let list = localStorage.getItem('items');
+    console.log(list)
+    if (list) {
+      return JSON.parse(localStorage.getItem('items'));
+    } else {
+      return data;
+    }
+  }
+
+  const [commentStructure, setCommentStructure] = useState(getLocalItems());
+  const [text, setText] = useState({ id: ++j, user: commentStructure.currentUser, replies: [] });
+  const [isPublish, setIsPublish] = useState(false);
+
+  useEffect(() => {
+    localStorage.setItem('items', JSON.stringify(commentStructure));
+  }, [commentStructure]);
+
+
+
+  function deleteReply(val) {
+
+    const newCommentStructure = { ...commentStructure };
+
+
+    function removeComment(obj) {
+      for (let x in obj) {
+        if (obj[x].id == val) {
+
+          obj.splice(x, 1);
+          break;
+        }
+        if (obj[x].replies !== undefined) {
+          removeComment(obj[x].replies);
+        }
+      }
+
+    }
+
+    removeComment(newCommentStructure.comments)
+
+    setCommentStructure(newCommentStructure);
+
+
+  }
+
+  function addReply(val) {
+    const newCommentStructure = { ...commentStructure };
+
+    if (isPublish) {
+      newCommentStructure.comments.push({ ...text })
+
+    } else {
+
+
+      for (let x of newCommentStructure.comments) {
+        if (x['id'] === val[1]) {
+          if (x['replies'].length == 0) {
+            x['replies'] = []
+          }
+          x['replies'].push(val[0]);
+          break;
+        }
+
+        for (let y of x['replies']) {
+          if (y['id'] === val[1]) {
+            x['replies'].push(val[0]);
+            break;
+          }
+        }
+
+      }
+
+    }
+
+
+
+    setCommentStructure(newCommentStructure);
+
+
+  }
+
+  function autoResize(e) {
+    e.target.style.height = "auto";
+    e.target.style.height = e.target.scrollHeight + "px";
+
+    const newText = { ...text };
+
+
+
+
+    newText["content"] = e.target.value;
+    setText(newText);
+  }
+
+  function addComment(e) {
+    if (e.keyCode == 13 && !e.shiftKey && e.target.value.length != 0) {
+
+      addReply([text, text.id])
+      e.target.value = ''
+
+    }
+
+    if (e.target.value.length != 0) {
+
+      setIsPublish(true);
+    } else if (e.target.value.length == 0) {
+      setIsPublish(false);
+    }
+
+
+  }
+
+  console.log(commentStructure);
+  return (<>
+    <div className="publish">
+      {'@' + commentStructure.currentUser.username}
+      <br />
+      <br />
+      <img
+        className="user-img"
+        src={commentStructure.currentUser.image.png.substr(2)}
+        alt="user"
+      />
+      <br />
+      <div className="reply new-reply" style={{ marginLeft: '340px' }}>
+
+        <div>
+          <textarea onInput={(e) => autoResize(e)} onKeyUp={addComment} autofocus>{
+
+            commentStructure.content
+
+          }</textarea>
+          <br />
+        </div>
+      </div></div>
+    <div>
+      {commentStructure.comments.map((val, index) => {
+        let value = <Comment id={val.id} data={val} deleteReply={deleteReply} addReply={addReply} currentUser={commentStructure.currentUser} />;
+
+        return val.replies === undefined || val.replies.length === 0 ? (
+          value
+        ) : (
+          // <div className="comment-parent-container-div">
+          <div>
+            <Comment data={val} id={val.id} addReply={addReply} deleteReply={deleteReply} currentUser={commentStructure.currentUser} />
+            {val.replies.map((value, i) => {
+
+              return <Comment id={value.id} data={value} addReply={addReply} deleteReply={deleteReply} currentUser={commentStructure.currentUser} />;
+            })}
+          </div>
+          // </div>
+        );
+      })}
+    </div></>
   );
 }
-
-export default App;
---
+++ src/Comment.js
@@ -0,0 +1,103 @@
+import "./styles.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faReply, faAngleUp } from '@fortawesome/free-solid-svg-icons'
+import React, { useRef, useState } from "react";
+import TextArea from "./Textarea";
+
+
+
+export default function Comment(props) {
+
+  const commentRef = useRef(null)
+  const [replyState, setReplyState] = useState(false);
+  const [score, setScore] = useState({ up: 0, down: 0 });
+  const [isEditing, setIsEditing] = useState(false);
+  const [id, setId] = useState(null)
+
+  function handleReplyState() {
+    setId(props.id);
+    setReplyState(!replyState);
+  }
+
+  function handleDown(e) {
+    setScore((prev) => {
+
+      return { down: score.down + 1, up: prev.up }
+    })
+
+  }
+
+  function handleUp(e) {
+    setScore((prev) => {
+
+      return { up: score.up + 1, down: prev.down }
+    })
+  }
+
+  function handleDelete() {
+
+    if (id !== null) {
+
+      setId(null)
+      setIsEditing(false)
+
+
+    }
+
+    props.deleteReply(id ? id : commentRef.current.id);
+
+
+
+
+
+  }
+
+
+
+  function handleEdit() {
+
+    commentRef.current.className = 'hide';
+    setIsEditing(true)
+
+    setReplyState(!replyState);
+
+  }
+
+
+
+  return (
+    <>
+      <div id={props.id}
+        ref={commentRef}
+        className={"comment-parent-container"}
+        style={props.data.replies === undefined ? { marginLeft: "40px" } : null}
+      >
+        <img
+          src={props.data.user.image.png.substr(2)}
+          alt="user"
+          style={
+            props.data.replies === undefined
+              ? { width: "25px", height: "25px" }
+              : null
+          }
+        />
+        <div className="comment-outer-container">
+          <div className="comment-container">
+            <b className="username">{props.data.user.username}</b>
+            {props.data.replyingTo === undefined ? null : <><span >  <FontAwesomeIcon icon={faReply} flip="horizontal" size="2xs" /></span>
+              <b className="username">{props.data.replyingTo}</b></>}
+
+
+            <div className="comment">{props.data.content}</div>
+            <FontAwesomeIcon className='icon' onClick={handleUp} icon={faAngleUp} size="2xs" />
+            <FontAwesomeIcon className='icon' onClick={handleDown} icon={faAngleUp} rotation={180} style={{ marginBottom: '.3px', marginLeft: '13px' }} size="2xs" /><br />
+            <span className="score">+{score.up}</span><span className="score" style={{ marginLeft: '27px' }}>-{score.down}</span>
+          </div>
+          {props.currentUser.username === props.data.user.username ? <><button onClick={handleEdit} >Edit</button><button onClick={handleDelete}>Delete</button></> : null}
+          <button onClick={() => handleReplyState()}>Reply</button>
+        </div>
+      </div>
+      {replyState ? <TextArea addReply={props.addReply} deleteReply={handleDelete} handleReplyState={handleReplyState} replyingTo={props.data.user.username} replyId={props.id} currentUser={props.currentUser} content={props.data.content} isEditing={isEditing} /> : null}
+    </>
+  );
+}
